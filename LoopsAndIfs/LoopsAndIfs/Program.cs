526f069730eff8fbf4a85edb3282bfbd327a84f4
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-09-20 19:40:45 +1200

    first commit

---
+++ LoopsAndIfs/LoopsAndIfs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAndIfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("*****************************************************************");
+            //Console.WriteLine("*************************** Task 1 ******************************");
+            //Console.WriteLine("*****************************************************************\n");
+
+            //Console.WriteLine("Odd numbers starting from 76 and ending @ 46: ");
+            //for(int n = 46; n < 76; n++)
+            //{
+            //    if(n % 2 != 0)
+            //    {
+            //        Console.WriteLine(n.ToString());
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("*************************** Task 1 ******************************");
+            Console.WriteLine("*****************************************************************\n");
+        }
+    }
+}
